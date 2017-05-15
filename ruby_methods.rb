b4dddf4f4d7b554696c3f303debaecaf479992cf
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-15 16:31:17 -0700

    solved loops 1 and 2

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -1,7 +1,25 @@
-dev p_times(statement,  num)
-	num.times do |variable|
-		puts statement
+# Looping - 1
+# def p_times(statement,  num)
+# 	num.times do |variable|
+# 		puts statement
+# 	end
+# end
+
+# p_times("Dave is a not polite!", 4)
+
+# Looping - 2
+def letter_count string
+	# turn into array
+	# find all letters - new_array
+	# loop through new_array, create object with letter in new_array loop as key, number times letter in first array as value
+	# return object
+	arr = string.downcase.split("")
+	uniq_ltrs = arr.uniq
+	hash_ltrs = {}
+	uniq_ltrs.each do |letter|
+		hash_ltrs[letter] = arr.count(letter)
 	end
+	hash_ltrs
 end
 
-p_times("Dave is a not polite!", 4)
+p letter_count("Jana")
