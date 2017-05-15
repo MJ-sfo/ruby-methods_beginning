f071ee0917594e7a4c061356881ce0fbb6b4909d
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-15 16:39:47 -0700

    loop 3

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -8,18 +8,30 @@
 # p_times("Dave is a not polite!", 4)
 
 # Looping - 2
-def letter_count string
-	# turn into array
-	# find all letters - new_array
-	# loop through new_array, create object with letter in new_array loop as key, number times letter in first array as value
-	# return object
-	arr = string.downcase.split("")
-	uniq_ltrs = arr.uniq
-	hash_ltrs = {}
-	uniq_ltrs.each do |letter|
-		hash_ltrs[letter] = arr.count(letter)
+# def letter_count string
+# 	# turn into array
+# 	# find all letters - new_array
+# 	# loop through new_array, create object with letter in new_array loop as key, number times letter in first array as value
+# 	# return object
+# 	arr = string.downcase.split("")
+# 	uniq_ltrs = arr.uniq
+# 	hash_ltrs = {}
+# 	uniq_ltrs.each do |letter|
+# 		hash_ltrs[letter] = arr.count(letter)
+# 	end
+# 	hash_ltrs
+# end
+
+# p letter_count("Jana")
+
+# Looping - 3
+def mock_me
+	input = ""
+	until input.downcase == "quit" || input.downcase == "q"
+		p "please type an input!"
+		input = gets.chomp.strip
+		puts input
 	end
-	hash_ltrs
 end
 
-p letter_count("Jana")
+mock_me
